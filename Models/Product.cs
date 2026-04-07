6ceb8ece36fc4ce31b0117dee5eccfa3cfa09bf3
zachschool1 <zacharyjordan@my.whccd.edu>
2026-04-06 18:10:33 -0700

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.LectureActivity3.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+
+        public string Name { get; set; }
+
+        [Range(0.01,10000)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+/*
+ * Author: Zachary Jordan
+ * Course: COMP003B: ASP.NET Core
+ * Faculty: Jonathan Cruz
+ * Purpose: Demonstates routing, model binding and validation in ASP.NET Core MVC.
+ */
+
+namespace COMP003B.LectureActivity3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
